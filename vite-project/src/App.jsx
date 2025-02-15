86d7564012dc02b189a265fb21fd708a348f0aba
Kavyadharshii <dharshi.kavya21@gmail.com>
2025-02-15 18:50:06 +0530

    second commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const myName = "Kavyadharshini S M";
+  const myAge = 20;
+  React.useEffect(() => {
+    alert("Hi");
+    console.log(`My age is ${myAge}`);
+  }, []);
 
   return (
-    <>
-     <H1>KavyadharshiniSM</H1>
-    </>
-  )
-}
+    <div className="name-container">
+      {myName}
+    </div>
+  );
+};
 
-export default App
+export default App;
